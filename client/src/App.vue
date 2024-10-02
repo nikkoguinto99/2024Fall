0cba3009fb40354887e32a0d141c72073b7cabae
Nikko <nikkoguinto@gmail.com>
2024-10-01 22:36:06 -0400

    Installed NavBar

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,19 +1,12 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
+import Navbar from './components/Navbar.vue';
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+    <Navbar />
 
-    <div class="wrapper">
-      <HelloWorld msg="Welcome to this page." />
-
-      <nav>
-        <RouterLink to="/">Meow</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
   </header>
 
   <RouterView />
